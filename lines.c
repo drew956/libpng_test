9dc271811e7a0d07652e2e5b0cd1efd88ece6282
Logan Haser <logandrew95@yahoo.com>
2023-05-07 23:10:19 -0700

    Fixed the code for the horizontal lines. Working on adding functions to clean up the main method before moving on to more complicated tasks

--- lines.c
+++ lines.c
@@ -5,7 +5,7 @@
 #include <png.h>
 
 #define WIDTH 256
-#define HEIGHT 32
+#define HEIGHT 256
 #define COLOR_DEPTH 8
 
 struct Pixel {
@@ -14,6 +14,7 @@ struct Pixel {
 
 void fillImageWithRandomVeritcalBars(struct Pixel** pixels, int width, int height);
 void fillImageWithRandomHorizontalBars(struct Pixel** pixels, int width, int height);
+void setFileNames(int argc, char* argv[], char* file_t1, char* file_t2, char* file_t3);
 
 int main(int argc, char *argv[]) {
 	srand(time(NULL)); //seed based on time to make it random each time
@@ -21,22 +22,15 @@ int main(int argc, char *argv[]) {
     char* file_t2 = "horizontal.png";
     char* file_t3 = "random.png";
     
-    switch(argc){
-        case 4:
-            file_t3 = argv[3];
-        case 3:
-            file_t2 = argv[2];
-        case 2:
-            file_t1 = argv[1];
-            break;
-    }
 
 	/* open PNG file for writing */
 	FILE *f1  = fopen(file_t1, "wb");
 	FILE *f2  = fopen(file_t2, "wb");
 	FILE *f3  = fopen(file_t3, "wb");
 
-	if (!f1 || !f2 || !f3) {
+    setFileNames(argc, argv, file_t1, file_t2, file_t3);
+	
+    if (!f1 || !f2 || !f3) {
 		fprintf(stderr, "could not open out.png\n");
 		return 1;
 	}
@@ -102,21 +96,20 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
+
+
+
+
 void fillImageWithRandomHorizontalBars(struct Pixel** pixels, int width, int height){
     for (int row = 0; row < height; row++) {
-		int bar_height = rand() % height;
+		int bar_height = rand() % width;
         int r = rand() % 256;
         int g = rand() % 256;
         int b = rand() % 256;
         int a = rand() % 256;
         
 	    for (int col = 0; col < width; col++) {
-			/* Since we go row by row (i.e. downwards through the image) 
-             * we check if the height - row (current pixel height) <= bar height
-             * If it is, we color the pixel accordingly.
-             * Each bar gets its own random color as the column is fixed when we move through the rows. 
-             * */
-            if (width - col <= bar_height) {
+            if (col <= bar_height) {
 				pixels[row][col].r = r; // red
 				pixels[row][col].g = g; // green
 				pixels[row][col].b = b; // blue
@@ -157,5 +150,17 @@ void fillImageWithRandomVeritcalBars(struct Pixel** pixels, int width, int heigh
 			}
 		}
 	}
+
+}
+void setFileNames(int argc, char* argv[], char* file_t1, char* file_t2, char* file_t3){
+    switch(argc){
+        case 4:
+            file_t3 = argv[3];
+        case 3:
+            file_t2 = argv[2];
+        case 2:
+            file_t1 = argv[1];
+            break;
+    }
 
 }
